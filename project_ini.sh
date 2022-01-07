5572f480a93372fd85f84182eb5adf5c3d76e153
Petros <petrucus@gmail.com>
2022-01-07 17:35:08 +0000

    update index.html
    change wget to curl for gitbash compatibility
    fixed population of index.html initial setup to the correct path
    changed Project name input to prompt
    Changed the api to github to create private repo
    Changed initial commit to everything instead of just the README

--- project_ini.sh
+++ project_ini.sh
@@ -3,10 +3,12 @@
 # The directory I choose to store all my SHA projects
 directory_to_use="/c/Users/pesy/OneDrive/Documents/SHA_FE"
 
-# The directory of the project itself
-full_path="$directory_to_use/$1"
+read -p "Project name: " project_name
 
-echo "Creating project $1 in $directory_to_use"
+# The directory of the project itself
+full_path="$directory_to_use/$project_name"
+
+echo "Creating project $project_name in $directory_to_use"
 
 # Confirming everything looks good so far
 read -p "Press enter to continue" gogo
@@ -27,7 +29,7 @@ touch $full_path/css/style.css
 touch $full_path/js/script.js
 
 # setting up index.html
-cat << EOF > index.html
+cat << EOF > $full_path/index.html
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -35,15 +37,15 @@ cat << EOF > index.html
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
         <link rel="icon" href="img/caticon.png">
         <link rel="stylesheet" href="/css/style.css" media="all">
-        <title>$1</title>
+        <title>$project_name</title>
     </head>
 </html>
 EOF
 
 # adding in imgs + fonts
-wget https://www.freefavicon.com/freefavicons/animal/cat--5-152-85909.png -O $full_path/img/caticon.png
+curl https://www.freefavicon.com/freefavicons/animal/cat--5-152-85909.png --output $full_path/img/caticon.png
 
-echo "Project $1 folders have been created"
+echo "Project $project_name folders have been created"
 
 # use tree to display and confirm folder-file structure
 tree $full_path || \
@@ -54,18 +56,19 @@ echo -e "and moving it to \e[4mC:\Program Files\Git\usr\'bin'\e[0m"
 
 # API POST call to github, to create a repo. Needs access token
 # To create a token see: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
-curl -X POST -u $github_access_token:x-oauth-basic https://api.github.com/user/repos -d "{\"name\":\"$1\"}"
+# To read more about github api calls see: https://docs.github.com/en/rest/guides/getting-started-with-the-rest-api
+curl -X POST -u $github_access_token:x-oauth-basic https://api.github.com/user/repos -d "{\"name\":\"$project_name\", \"private\": true}"
 
 # Setting up the repo README
-echo "# $1" > $full_path/README.md
+echo "# $project_name"|sed 's/_/ /g' > $full_path/README.md
 cd $full_path
 
 # Initializing the repo locally
 git init
 
 # Adding, commiting and pushing the changes from local to remote
-git add README.md
+git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/Petrucus/$1.git
+git remote add origin https://github.com/Petrucus/$project_name.git
 git push -u origin main
